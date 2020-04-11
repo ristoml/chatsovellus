b4dc675d0691cfc6998d1ca3cbfbfa8d175cd5e1
Sami Arola <sami.arola6@gmail.com>
2020-04-11 16:55:15 +0300

    testing router

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,13 @@ const userRouter = require('./controllers/users')(pool);
 
 app.use(cors());
 app.use(bodyParser.json());
+
 app.use('/api/users', userRouter);
 
-module.exports = app;
+if (process.env.NODE_ENV === 'development') {
+  const testRouter = require('./controllers/tests')(pool);
+  app.use('/api/tests', testRouter);
+}
+
+module.exports = { app, pool };
 
