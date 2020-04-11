3cce750d912ea9a93836e1c4378baca7cd8ca248
Sami Arola <sami.arola6@gmail.com>
2020-04-11 18:02:37 +0300

    tests for username, user type and real name

--- backend/tests/user_api.test.js
+++ backend/tests/user_api.test.js
@@ -46,17 +46,22 @@ describe('When there is initially one user in the database', () => {
     expect(rowsAfter.map((u) => u.username)).toContain('testuser');
     expect(rowsAfter.map((u) => u.username)).toContain('root');
 
-    expect(rowsAfter).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining(userWithoutPass)
-      ])
-    );
+    const userOfTypeUser = {
+      username: 'anotheruser',
+      realname: 'Kalevi',
+      type: 'user',
+      password: 'abcd123!',
+    }
 
-    expect(rowsAfter).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining(initialUserWithoutPass)
-      ])
-    );
+    await api.post('/api/users').send(userOfTypeUser).expect(201);
+
+    const usersAfter2 = await api.get('/api/users');
+    const rowsAfter2 = usersAfter2.body;
+
+    expect(rowsAfter2.length).toBe(rowsBefore.length + 2);
+    expect(rowsAfter2.map((u) => u.username)).toContain('testuser');
+    expect(rowsAfter2.map((u) => u.username)).toContain('root');
+    expect(rowsAfter2.map((u) => u.username)).toContain('anotheruser');
   });
 
   test('user without a proper password cannot be added to the database', async () => {
@@ -72,16 +77,97 @@ describe('When there is initially one user in the database', () => {
       password: '123223982',
       ...userWithoutPass
     };
+    const onlyWhiteSpace = {
+      password: '                 ',
+      ...userWithoutPass
+    };
 
     const res1 = await api.post('/api/users').send(userWithoutPass).expect(400);
     const res2 = await api.post('/api/users').send(tooShort).expect(400);
     const res3 = await api.post('/api/users').send(noDigits).expect(400);
     const res4 = await api.post('/api/users').send(noLowerCaseLetters).expect(400);
+    const res5 = await api.post('/api/users').send(onlyWhiteSpace).expect(400);
 
     expect(res1.body.error).toMatch(/password is required/i);
     expect(res2.body.error).toMatch(/password length should be at least 8 characters long/i);
     expect(res3.body.error).toMatch(/password shorter than 16 characters should contain at least one digit/i);
     expect(res4.body.error).toMatch(/password shorter than 16 characters should contain at least one lowercase letter/i);
+    expect(res5.body.error).toMatch(/password is required/i);
+  });
+
+  test('user without a username cannot be added', async () => {
+    const noUserName = {
+      ...userWithoutPass,
+      password: '2383289382932892389ugrg',
+      username: undefined
+    };
+    const onlyWhiteSpace = {
+      ...userWithoutPass,
+      password: '2383289382932892389ugrg',
+      username: '                               '
+    };
+    const lessThanFiveChars = {
+      ...userWithoutPass,
+      password: '2383289382932892389ugrg',
+      username: 'abcd'
+    };
+
+    const res1 = await api.post('/api/users').send(noUserName).expect(400);
+    const res2 = await api.post('/api/users').send(onlyWhiteSpace).expect(400);
+    const res3 = await api.post('/api/users').send(lessThanFiveChars).expect(400);
+    expect(res1.body.error).toMatch(/username is required/i);
+    expect(res2.body.error).toMatch(/username is required/i);
+    expect(res3.body.error).toMatch(/username should be at least 5 characters long/i);
+  });
+
+  test('user without a real name cannot be added', async () => {
+    const noUserName = {
+      ...userWithoutPass,
+      password: '2383289382932892389ugrg',
+      realname: undefined
+    };
+    const onlyWhiteSpace = {
+      ...userWithoutPass,
+      password: '2383289382932892389ugrg',
+      realname: '                               '
+    };
+    const lessThanFiveChars = {
+      ...userWithoutPass,
+      password: '2383289382932892389ugrg',
+      realname: 'abcd'
+    };
+
+    const res1 = await api.post('/api/users').send(noUserName).expect(400);
+    const res2 = await api.post('/api/users').send(onlyWhiteSpace).expect(400);
+    const res3 = await api.post('/api/users').send(lessThanFiveChars).expect(400);
+    expect(res1.body.error).toMatch(/real name is required/i);
+    expect(res2.body.error).toMatch(/real name is required/i);
+    expect(res3.body.error).toMatch(/real name should be at least 5 characters long/i);
+  });
+
+  test('user without a proper user type cannot be added', async () => {
+    const noType = {
+      ...userWithoutPass,
+      password: '2383289382932892389ugrg',
+      type: undefined
+    };
+    const notProperType1 = {
+      ...userWithoutPass,
+      password: '2383289382932892389ugrg',
+      type: '                               '
+    };
+    const notProperType2 = {
+      ...userWithoutPass,
+      password: '2383289382932892389ugrg',
+      type: 'abcd'
+    };
+
+    const res1 = await api.post('/api/users').send(noType).expect(400);
+    const res2 = await api.post('/api/users').send(notProperType1).expect(400);
+    const res3 = await api.post('/api/users').send(notProperType2).expect(400);
+    expect(res1.body.error).toMatch(/user type is required/i);
+    expect(res2.body.error).toMatch(/user type is required/i);
+    expect(res3.body.error).toMatch(/not a proper user type/i);
   });
 });
 
