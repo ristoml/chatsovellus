d2784e3797f94570f26a9cfb044e590ef16c6e54
Sami Arola <sami.arola6@gmail.com>
2020-04-23 06:28:36 +0300

    more test routes

--- backend/controllers/tests.js
+++ backend/controllers/tests.js
@@ -3,7 +3,7 @@ const db     = require('../db');
 const router = require('express').Router();
 
 router.post('/resetusers', async (_request, response) => {
-  db.query('DELETE FROM users');
+  await db.query('DELETE FROM users');
   response.status(204).end();
 });
 
@@ -12,10 +12,64 @@ router.post('/addtestuser', async (_request, response) => {
   const saltRounds = 10;
   const hash = await bcrypt.hash(testPass, saltRounds);
 
-  db.query('INSERT INTO users (username, realname, passwordhash, type) VALUES ($1, $2, $3, $4)',
+  await db.query('INSERT INTO users (username, realname, passwordhash, type) VALUES ($1, $2, $3, $4)',
     ['root', 'root', hash, 'admin']);
 
   response.status(204).end();
+});
+
+router.post('/addsometestusers', async (_request, response, next) => {
+  const users = [
+    {
+      username: 'user1',
+      realname: 'user1',
+      password: 'testpass1',
+      type: 'admin'
+    },
+    {
+      username: 'user2',
+      realname: 'user2',
+      password: 'testpass2',
+      type: 'admin'
+    },
+    {
+      username: 'user3',
+      realname: 'user3',
+      password: 'testpass3',
+      type: 'admin'
+    },
+    {
+      username: 'user4',
+      realname: 'user4',
+      password: 'testpass4',
+      type: 'user'
+    },
+    {
+      username: 'user5',
+      realname: 'user5',
+      password: 'testpass5',
+      type: 'user'
+    },
+  ];
+
+  const promiseArray = users.map((u) => {
+    const saltRounds = 10;
+
+    return bcrypt.hash(u.password, saltRounds)
+      .then((hash) => {
+        db.query(
+          'INSERT INTO users (username, realname, passwordhash, type) VALUES ($1, $2, $3, $4)',
+          [u.username, u.realname, hash, u.type]
+        );
+      });
+  });
+
+  try {
+    await Promise.all(promiseArray);
+    response.status(204).end();
+  } catch (expect) {
+    next(expect);
+  }
 });
 
 module.exports = router;
