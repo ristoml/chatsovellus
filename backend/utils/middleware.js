b56e90c366df67fd0d66af5fa58f77097c15a92b
Sami Arola <sami.arola6@gmail.com>
2020-04-23 06:29:48 +0300

    error handler

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -24,4 +24,12 @@ const logger = () => {
   return morgan(`${minimal} :body`);
 };
 
-module.exports = { logger };
+const errorHandler = (error, request, response, next) => {
+  if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: 'invalid token' });
+  }
+
+  next(error);
+};
+
+module.exports = { logger, errorHandler };
