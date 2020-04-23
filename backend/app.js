c6932cbed0859f349cce1eccba532636fe371f36
Sami Arola <sami.arola6@gmail.com>
2020-04-23 18:08:46 +0300

    change swagger configuration

--- backend/app.js
+++ backend/app.js
@@ -3,22 +3,8 @@ const bodyParser   = require('body-parser');
 const config       = require('./utils/config');
 const cors         = require('cors');
 const middleware   = require('./utils/middleware');
-const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerDocs  = require('./swagger.json');
 const swaggerUi    = require('swagger-ui-express');
-
-const swaggerOptions = {
-  swaggerDefinition: {
-    info: {
-      title: 'Chat API',
-      description: 'Chat application API',
-      servers: [`http://localhost:${config.PORT}`]
-    }
-  },
-  apis: ['./controllers/*.js'] 
-};
-
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -35,6 +21,8 @@ if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
   const testRouter = require('./controllers/tests');
   app.use('/api/tests', testRouter);
 }
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(middleware.errorHandler);
 
