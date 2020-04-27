d31046512960e8f70546cc04b800f6471c0bf63b
Sami Arola <sami.arola6@gmail.com>
2020-04-28 00:54:23 +0300

    move routes to its own file

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,26 +2,14 @@ const app         = require('express')();
 const bodyParser  = require('body-parser');
 const cors        = require('cors');
 const middleware  = require('./utils/middleware');
-const swaggerDocs = require('../swagger.json');
-const swaggerUi   = require('swagger-ui-express');
+const mountRoutes = require('./controllers');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(middleware.tokenExtractor);
 app.use(middleware.logger());
 
-const userRouter  = require('./controllers/users');
-const loginRouter = require('./controllers/login');
-
-app.use('/api/users', userRouter);
-app.use('/api/login', loginRouter);
-
-if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
-  const testRouter = require('./controllers/tests');
-  app.use('/api/tests', testRouter);
-}
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+mountRoutes(app);
 
 app.use(middleware.errorHandler);
 
---
+++ backend/src/controllers/index.js
@@ -0,0 +1,19 @@
+const userRouter  = require('./users');
+const loginRouter = require('./login');
+const testRouter  = require('./tests');
+const swaggerDocs = require('../../swagger.json');
+const swaggerUi   = require('swagger-ui-express');
+
+const mountRoutes = (app) => {
+  app.use('/api/users', userRouter);
+  app.use('/api/login', loginRouter);
+
+  if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
+    app.use('/api/tests', testRouter);
+  }
+
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+};
+
+module.exports = mountRoutes;
+
