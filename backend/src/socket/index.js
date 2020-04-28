3aafab13ddff54f613c1eab3e9615588cdb66119
Sami Arola <sami.arola6@gmail.com>
2020-04-28 16:15:26 +0300

    modify events

--- backend/src/socket/index.js
+++ backend/src/socket/index.js
@@ -6,21 +6,46 @@ const init = (server) => {
   const io = socketio(server);
 
   io.on('connection', (socket) => {
-    sockets[socket.id] = socket;
     console.log(`socket ${socket.id} connected`);
 
-    socket.on('new-user', () => {
-      io.emit('user-list', Object.keys(sockets));
+    socket.on('new-user', (username) => {
+      if (typeof username !== 'string') {
+        socket.emit(
+          'error',
+          `ERROR: expected a string but was ${typeof message} for event 'new-user'.`
+        );
+        return;
+      }
+      sockets[socket.id] = { socket, username };
+      const users =
+        Object.values(sockets)
+          .map((v) => v.username);
+
+      io.emit('user-list', Object.keys(users));
     });
 
     socket.on('new-message', (message) => {
+      if (typeof message !== 'string') {
+        socket.emit(
+          'error',
+          `ERROR: expected a string but was ${typeof message} for event new-message.`
+        );
+        return;
+      }
+      const youmsg = 'You: ' + message;
+      socket.emit('new-message', youmsg);
       socket.broadcast.emit('new-message', message);
     });
 
     socket.on('disconnect', () => {
       console.log(`socket ${socket.id} disconnected`);
       delete sockets[socket.id];
-      io.emit('user-list', Object.keys(sockets));
+
+      const users =
+        Object.values(sockets)
+          .map((v) => v.username);
+
+      io.emit('user-list', users);
     });
   });
 };
