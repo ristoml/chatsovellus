2bac9be939e4a673374416aab885cf815200a4eb
Sami Arola <sami.arola6@gmail.com>
2020-04-30 00:28:32 +0300

    remove post

--- backend/src/controllers/chat.js
+++ backend/src/controllers/chat.js
@@ -1,5 +1,4 @@
 const db     = require('../db');
-const jwt    = require('jsonwebtoken');
 const router = require('express').Router();
 
 router.get('/', async (request, response, next) => {
@@ -18,37 +17,6 @@ router.get('/', async (request, response, next) => {
     response
       .status(200)
       .json(rows);
-  } catch (except) {
-    next(except);
-  }
-});
-
-router.post('/', async (request, response, next) => {
-  const { message } = request.body;
-  const { token } = request;
-
-  if (!message) {
-    return response.status(400).json({ error: 'Missing field: \'message\'' });
-  }
-
-  try {
-    const decodedToken = await jwt.verify(token, process.env.SECRET);
-
-    if (!decodedToken.username || !decodedToken.type) {
-      return response.status(401).json({ error: 'invalid token' });
-    }
-    const date = new Date();
-    const [ m, d, y ] = date.toLocaleDateString().split('/');
-    const time = date.toTimeString().split(' ')[0];
-
-    const { id } = decodedToken;
-    const created = `${y}-${m}-${d} ${time}`;
-
-    await db.query(
-      'INSERT INTO messages (userid, message, created) VALUES ($1, $2, $3)',
-      [id, message, created]
-    );
-    response.status(201).end();
   } catch (except) {
     next(except);
   }
