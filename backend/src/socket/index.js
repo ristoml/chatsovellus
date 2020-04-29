58a3fce6d0a98e871d00f5f213998328e935d4a2
Sami Arola <sami.arola6@gmail.com>
2020-04-30 00:29:20 +0300

    fix timestamp problem

--- backend/src/socket/index.js
+++ backend/src/socket/index.js
@@ -1,87 +1,87 @@
-const db = require('../db');
+const db       = require('../db');
 const socketio = require('socket.io');
 
 const sockets = {};
 
-const handleMessage = async(socket, data) => {
-    const { id, username, message } = data;
+const handleMessage = async (socket, data) => {
+  const { id, username, message } = data;
 
-    const date = new Date();
-    const [m, d, y] = date.toLocaleDateString().split('/');
-    const time = date.toTimeString().split(' ')[0];
+  const date = new Date();
+  const [ m, d, y ] = [ date.getUTCMonth()+1, date.getUTCDate(), date.getUTCFullYear() ];
+  const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 
-    //const created = `${y}-${m}-${d} ${time}`;
-    const created = '2020-01-01 ' + time;
+  const created = `${y}-${m}-${d} ${time}`;
 
-    await db.query(
-        'INSERT INTO messages (userid, message, created) VALUES ($1, $2, $3)', [id, message, created]
-    );
+  await db.query(
+    'INSERT INTO messages (userid, message, created) VALUES ($1, $2, $3)',
+    [id, message, created]
+  );
 
-    const youmsg = 'You: ' + message;
-    const msg = `${username}: ${message}`;
-    await socket.emit('newMessage', youmsg);
-    await socket.broadcast.emit('newMessage', msg);
+  const youmsg = `You: ${message}`;
+  const msg    = `${username}: ${message}`;
+  await socket.emit('newMessage', youmsg);
+  await socket.broadcast.emit('newMessage', msg);
 };
 
 const init = (server) => {
-    const io = socketio(server);
+  const io = socketio(server);
 
-    io.on('connection', (socket) => {
-        console.log(`socket ${socket.id} connected`);
+  io.on('connection', (socket) => {
+    console.log(`socket ${socket.id} connected`);
 
-        socket.on('newUser', (username) => {
-            if (typeof username !== 'string') {
-                socket.emit(
-                    'error',
-                    `ERROR: expected a string but was ${typeof message} for event 'new-user'.`
-                );
-                return;
-            }
-            sockets[socket.id] = { socket, username };
-            const users =
-                Object.values(sockets)
-                .map((v) => v.username);
+    socket.on('newUser', (username) => {
+      if (typeof username !== 'string') {
+        socket.emit(
+          'error',
+          `ERROR: expected a string but was ${typeof message} for event 'newUser'.`
+        );
+        return;
+      }
+      sockets[socket.id] = { socket, username };
+      const users =
+        Object.values(sockets)
+          .map((v) => v.username);
 
-            io.emit('user-list', users);
-        });
-
-        socket.on('newMessage', async(data) => {
-            if (typeof data !== 'object') {
-                socket.emit(
-                    'error',
-                    `expected an object but was ${typeof data} for event new-message.`
-                );
-                return;
-            }
-            const { id, username, message } = data;
-            if (!id) {
-                socket.emit('error', 'missing id');
-                return;
-            }
-            if (!message) {
-                socket.emit('error', 'missing message');
-                return;
-            }
-            if (!username) {
-                socket.emit('error', 'missing username');
-                return;
-            }
-            await handleMessage(socket, data);
-        });
-
-        socket.on('disconnect', () => {
-            console.log(`socket ${socket.id} disconnected`);
-            delete sockets[socket.id];
-
-            const users =
-                Object.values(sockets)
-                .map((v) => v.username);
-
-            io.emit('user-list', users);
-        });
+      io.emit('userList', users);
     });
 
-    return io;
+    socket.on('newMessage', async(data) => {
+      if (typeof data !== 'object') {
+        socket.emit(
+          'error',
+          `expected an object but was ${typeof data} for event 'newMessage'.`
+        );
+        return;
+      }
+      const { id, username, message } = data;
+      if (!id) {
+        socket.emit('error', 'missing id');
+        return;
+      }
+      if (!message) {
+        socket.emit('error', 'missing message');
+        return;
+      }
+      if (!username) {
+        socket.emit('error', 'missing username');
+        return;
+      }
+      await handleMessage(socket, data);
+    });
+
+    socket.on('disconnect', () => {
+      console.log(`socket ${socket.id} disconnected`);
+      delete sockets[socket.id];
+
+      const users =
+        Object.values(sockets)
+          .map((v) => v.username);
+
+      io.emit('userList', users);
+    });
+  });
+
+  return io;
 };
 
 module.exports = init;
