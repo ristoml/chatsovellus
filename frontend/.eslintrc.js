20c666fc22966115a7e2131475bc3e564101bc22
Sami Arola <sami.arola6@gmail.com>
2020-05-03 22:57:31 +0300

    added more rules

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
     'indent': [
       2,
       2,
-      {'SwitchCase': 1}
+      { 'SwitchCase': 1 }
     ],
     'quotes': [
       'error',
@@ -37,7 +37,38 @@ module.exports = {
     'comma-spacing': ['error', { 'before': false, 'after': true }],
     'key-spacing': ['error', {  'afterColon': true, 'beforeColon': false }],
     'space-before-blocks': 'error',
-    'keyword-spacing': ['error', { 'before': true, 'after': true }]
+    'keyword-spacing': ['error', { 'before': true, 'after': true }],
+
+
+    'no-var': 'error',
+    'prefer-const': ['error', {
+      'destructuring': 'any',
+      'ignoreReadBeforeAssign': false
+    }],
+    'no-use-before-define': 'error',
+    'no-await-in-loop': 'error',
+
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-parens': ['error', 'always'],
+    'brace-style': 'error',
+    'camelcase': 'error',
+    'comma-style': ['error', 'last'],
+    'semi-spacing': 'error',
+    'switch-colon-spacing': ['error', { 'after': true, 'before': false }],
+    'array-callback-return': ['error', { 'allowImplicit': true }],
+    'curly': 'error',
+    'dot-notation': 'error',
+    'space-before-function-paren': [
+      'error', { 'anonymous': 'always', 'named': 'never', 'asyncArrow': 'always' }
+    ],
+    'no-constant-condition': 'error',
+    'no-extra-semi': 'error',
+    'no-useless-return': 'error',
+    'require-await': 'error',
+    'no-confusing-arrow': 'error',
+    'object-shorthand': ['error', 'always']
   },
 
   overrides: [{
