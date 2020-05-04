b440728c6acfe846355753f8547827ced02982fd
Risto Leivo <Ristomatti.Leivo@metropolia.fi>
2020-05-04 16:36:15 +0300

    join/left working

--- frontend/src/components/ChatItem.vue
+++ frontend/src/components/ChatItem.vue
@@ -2,7 +2,7 @@
   <span slot="activator">
     <p class="grey--text text--darken-2">
       <span>[ {{ message.created.split(' ')[1] }} ] </span>
-      <b class="indigo--text" v-if="message.username !== this.$store.getters.getUser.username && message.message==='joined' || message.message==='left'">{{message.username}} </b>
+      <b class="indigo--text" v-if="message.message==='joined' || message.message==='left'">{{message.username}} </b>
       <b class="indigo--text" v-else>{{message.username}} :</b>
       {{ message.message }}
     </p>
